f6687f96bf8e25287fd229a00ab53a799fe6f694
chrystalme <afam_ifeanyi@live.com>
2021-06-21 11:36:25 +0300

    Update desktop view and color

--- js/main.js
+++ js/main.js
@@ -1,8 +1,15 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
+const contact = document.querySelector(".nav-link");
 
 mobileMenu = () => {
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
 };
+
+linkClicked = () => {
+  console.log(clicked);
+  navMenu.classList.toggle("active");
+};
 hamburger.addEventListener("click", mobileMenu);
+contact.addEventListener("click", linkClicked);
